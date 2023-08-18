0abc387263b36b4faa6e2a7a4aa8d27a2b466e20
MrLawrenceAwe <mrlawrenceawe@gmail.com>
2023-08-18 09:49:49 +0100

    Initial commit

---
+++ src/main/java/Game.java
@@ -0,0 +1,12 @@
+public class Game {
+    private Board board = new Board();
+    private boolean gameOver;
+    private boolean playerWon;
+
+    public Game() {
+
+    }
+
+    public void start() {
+    }
+}
